03c6f113d384cb49f97d7a8b903ebb04dc99667f
kotala <kotarolab@gmail.com>
2018-01-31 14:55:52 +0900

    継承を使うためにclassに変更

--- TDD_BOOK/Doller.swift
+++ TDD_BOOK/Doller.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Doller {
+class Doller {
 
     private let amount: Int
 
--- TDD_BOOK/Franc.swift
+++ TDD_BOOK/Franc.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Franc {
+class Franc {
 
     private let amount: Int
 
