c06fad4b4ae1885135a99f30d79097bf9d91f189
kotala <kotarolab@gmail.com>
2018-02-14 08:52:57 +0900

    Add testcase

--- Section2/xunit.py
+++ Section2/xunit.py
@@ -1,13 +1,27 @@
-class WasRun:
+class TestCase:
+	def __init__(self, name):
+		self.name = name
+	def run(self):
+		method = getattr(self, self.name)
+		method()
+
+class WasRun(TestCase):
 	def __init__(self, name):
 		self.wasRun = None
-	def run(self):
-		self.testMethod()
+		super().__init__(name)
 	def testMethod(self):
 		self.wasRun = 1
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+# test = WasRun("testMethod")
+# print(test.wasRun)
+# test.run()
+# print(test.wasRun)
 
+class TestCaseTest(TestCase):
+	def testRunning(self):
+		test = WasRun("testMethod")
+		assert(not test.wasRun)
+		test.run()
+		assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
