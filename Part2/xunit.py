3b66ccc6670d87bc3f515904fab1fb98a4cec032
kotala <kotarolab@gmail.com>
2018-02-14 09:28:25 +0900

    tearDownを追加

--- Part2/xunit.py
+++ Part2/xunit.py
@@ -3,16 +3,21 @@ class TestCase:
 		self.name = name
 	def setUp(self):
 		pass
+	def tearDown(self):
+		pass
 	def run(self):
 		self.setUp()
 		method = getattr(self, self.name)
 		method()
+		self.tearDown()
 
 class WasRun(TestCase):
 	def setUp(self):
 		self.log = "setUp "
 	def testMethod(self):
 		self.log = self.log + "testMethod "
+	def tearDown(self):
+		self.log = self.log + "tearDown "
 
 # test = WasRun("testMethod")
 # print(test.wasRun)
@@ -20,10 +25,9 @@ class WasRun(TestCase):
 # print(test.wasRun)
 
 class TestCaseTest(TestCase):
-	def setUp(self):
-		self.test = WasRun("testMethod")
 	def testTemplateMethod(self):
-		self.test.run()
-		assert("setUp testMethod " == self.test.log)
+		test = WasRun("testMethod")
+		test.run()
+		assert("setUp testMethod tearDown " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
