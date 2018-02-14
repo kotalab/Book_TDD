0a6d24057951122bdd737e4dd01ddd4a87959040
kotala <kotarolab@gmail.com>
2018-02-14 09:06:45 +0900

    simplyfy test method

--- Section2/xunit.py
+++ Section2/xunit.py
@@ -9,10 +9,8 @@ class TestCase:
 		method()
 
 class WasRun(TestCase):
-	def __init__(self, name):
-		self.wasRun = None
-		super().__init__(name)
 	def setUp(self):
+		self.wasRun = None
 		self.wasSetUp = 1
 	def testMethod(self):
 		self.wasRun = 1
@@ -23,15 +21,14 @@ class WasRun(TestCase):
 # print(test.wasRun)
 
 class TestCaseTest(TestCase):
+	def setUp(self):
+		self.test = WasRun("testMethod")
 	def testRunning(self):
-		test = WasRun("testMethod")
-		assert(not test.wasRun)
-		test.run()
-		assert(test.wasRun)
+		self.test.run()
+		assert(self.test.wasRun)
 	def testSetUp(self):
-		test = WasRun("testMethod")
-		test.run()
-		assert(test.wasSetUp)
+		self.test.run()
+		assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
