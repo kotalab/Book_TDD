ec68b440c1e41ce7c440ae6cb86136d482b986e5
kotala <kotarolab@gmail.com>
2018-02-15 09:02:35 +0900

    小さなテストを追加し失敗件数をカウントできるようにした

--- Part2/xunit.py
+++ Part2/xunit.py
@@ -1,3 +1,14 @@
+class TestResult:
+	def __init__(self):
+		self.runCount = 0
+		self.errorCount = 0
+	def testStarted(self):
+		self.runCount = self.runCount + 1
+	def testFailed(self):
+		self.errorCount = self.errorCount + 1
+	def summary(self):
+		return "%d run, %d failed" % (self.runCount, self.errorCount)
+
 class TestCase:
 	def __init__(self, name):
 		self.name = name
@@ -9,10 +20,13 @@ class TestCase:
 		result = TestResult()
 		result.testStarted()
 		self.setUp()
-		method = getattr(self, self.name)
-		method()
+		try:
+			method = getattr(self, self.name)
+			method()
+		except:
+			result.testFailed()
 		self.tearDown()
-		return TestResult()
+		return result
 
 class WasRun(TestCase):
 	def setUp(self):
@@ -24,14 +38,6 @@ class WasRun(TestCase):
 	def tearDown(self):
 		self.log = self.log + "tearDown "
 		
-class TestResult:
-	def __init__(self):
-		self.runCount = 1
-	def testStarted(self):
-		self.runCount = self.runCount + 1
-	def summary(self):
-		return "%d run, 0 failed" % self.runCount
-
 class TestCaseTest(TestCase):
 	def testTemplateMethod(self):
 		test = WasRun("testMethod")
@@ -42,10 +48,16 @@ class TestCaseTest(TestCase):
 		result = test.run()
 		assert("1 run, 0 failed" == result.summary())
 	def testFailedResult(self):
-		test = WasRun("testMethod")
+		test = WasRun("testBrokenMethod")
 		result = test.run()
-		assert("1 run 1 failed" == result.summary())
+		assert("1 run, 1 failed" == result.summary())
+	def testFailedResultFormatting(self):
+		result = TestResult()
+		result.testStarted()
+		result.testFailed()
+		assert("1 run, 1 failed" == result.summary())
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-#TestCaseTest("testFailedResult").run()
+print(TestCaseTest("testTemplateMethod").run().summary())
+print(TestCaseTest("testResult").run().summary())
+print(TestCaseTest("testFailedResult").run().summary())
+print(TestCaseTest("testFailedResultFormatting").run().summary())
