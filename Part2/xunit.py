ae18ce3774138dda24999e5c23f08f2a7574383d
kotala <kotarolab@gmail.com>
2018-02-15 09:10:26 +0900

    TestSuiteクラスを追加

--- Part2/xunit.py
+++ Part2/xunit.py
@@ -1,3 +1,14 @@
+class TestSuite:
+	def __init__(self):
+		self.tests = []
+	def add(self, test):
+		self.tests.append(test)
+	def run(self):
+		result = TestResult()
+		for test in self.tests:
+			test.run(result)
+		return result
+
 class TestResult:
 	def __init__(self):
 		self.runCount = 0
